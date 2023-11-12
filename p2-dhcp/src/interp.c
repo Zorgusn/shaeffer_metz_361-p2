9b3afd319475fa7b7293dc9748c69d868e05f3e6
Zane Metz <zanem@cox.net>
2023-11-12 15:53:13 -0500

    Read file and process in interp

--- p2-dhcp/src/interp.c
+++ p2-dhcp/src/interp.c
@@ -1,28 +1,40 @@
 #include <stdlib.h>
+#include <stdio.h>
+#include <sys/stat.h>
 
 #include "dhcp.h"
 #include "format.h"
 
+
 int
 main (int argc, char **argv)
 {
+  msg_t packet;
   // TODO: Open the file specified on the command line
   char *filename = argv[1];
+  FILE *bin = fopen(filename, "r");
   
   // Note: The file contains a binary network packet
 
   // TODO: Determine the size of the packet in bytes
   // Note: You have used the fstat() function before
+  struct stat stats;
+  fstat(fileno(bin), &stats);
+  size_t size = stats.st_size;
 
   // TODO: Define a uint8_t * variable
+  // uint8_t *var;
 
   // TODO: Allocate enough space to hold the packet (store in your varaible)
+  msg_t packet = calloc(1, size);
 
   // TODO: Read the packet data from the file into your variable
+  fread(&packet, size, 1, bin);
 
   // TODO: Call dump_msg() from format.c to print the packet data to stdout
-
+  dump_msg(stdout, &packet, size);
   // TODO: Don't forget to free the dynamically-allocated space
+  free(packet);
 
   return EXIT_SUCCESS;
 }
