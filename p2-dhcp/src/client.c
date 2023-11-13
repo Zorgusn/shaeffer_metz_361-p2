adbb19fd27de55f4db0f09e2b224bf1b50cd145e
Zane Metz <zanem@cox.net>
2023-11-12 19:31:22 -0500

    Copy framework for arg handling in client

--- p2-dhcp/src/client.c
+++ p2-dhcp/src/client.c
@@ -7,10 +7,28 @@
 int
 main (int argc, char **argv)
 {
+  if (!get_args (argc, argv))
+    {
+      return EXIT_FAILURE;
+    }
   return EXIT_SUCCESS;
 }
+
 static bool
 get_args (int argc, char **argv)
 {
+  int option;
+
+  while ((option = getopt (argc, argv, "x:t:c:m:s:r:p")) != -1)
+    {
+      switch (option)
+        {
+        case 'p':
+          //nothing for now
+          break;
+        default:
+          return false;
+        }
+    }
   return true;
 }
